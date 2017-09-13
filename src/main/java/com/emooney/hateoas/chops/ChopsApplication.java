1a51067e85bce1943dad58ca89c1cf315f9448f7
Eric Mooney <eric.mooney@gmail.com>
2017-09-13 15:23:41 -0400

    force jenkins

--- src/main/java/com/emooney/hateoas/chops/ChopsApplication.java
+++ src/main/java/com/emooney/hateoas/chops/ChopsApplication.java
@@ -8,7 +8,7 @@ public class ChopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChopsApplication.class, args);
-		System.out.println("Hi World");
+		System.out.println("Hello World");
 	}
 
 
